3e81e3aa52966e286387f869db0597f4002ab5d1
priyanshu kumar <114394901+priyanshu-rj@users.noreply.github.com>
2025-03-23 23:24:47 +0530

    Update AuthContext.js

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -12,7 +12,7 @@ export const AuthProvider = ({ children }) => {
 
   const login = async (email, password) => {
     try {
-      const response = await fetch("http://localhost:5000/login", {
+      const response = await fetch("https://backend-tftv.onrender.com/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
